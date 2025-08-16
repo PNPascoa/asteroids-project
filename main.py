5f7192adadb04450bc17888715da9a85c4619039
PNPASCOA <thatguyonthecorner2@gmail.com>
2025-08-16 19:57:12 +0100

    finished(for now) game

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import sys
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
@@ -8,7 +13,9 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     x,y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
-    player = Player(x,y, PLAYER_RADIUS)
+    player = Player(x,y)
+    asteroid_field = AsteroidField()
+    player
   
     while True: 
         for event in pygame.event.get():
@@ -16,7 +23,14 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
-        
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    asteroid.split()
+                    shot.kill()
         for obj in drawable:
             obj.draw(screen)
         
@@ -24,13 +38,17 @@ def main():
         dt = clock.tick(60) / 1000
 
 
-
+##Groups
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
-
+asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
+#Containers
 Player.containers = (updatable, drawable)
-
-
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable)
+Shot.containers = (shots, updatable, drawable)
+##
 if __name__ == "__main__":
     main()
